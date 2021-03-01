2f4784305714d33ee899a2827081aed09b1e0825
Kajal Virmani <kajalvirmani24@gmail.com>
2021-03-01 20:04:28 +0530

    webpack changes

--- assignment8/webpack.config.js
+++ assignment8/webpack.config.js
@@ -1,39 +1,42 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
- "mode": "none",
- "entry": "./src/index.js",
- 
- "output": {
-   "path": __dirname + '/dist',
-   "filename": "bundle.js"
- },
-devServer: {
-   contentBase: path.join(__dirname, 'dist')
- },
- devtool : "cheap-module-source-map",
- "module": {
-  "rules": [
-    {
-      "test": /\.css$/,
-      "use": [
-        "style-loader",
-        "css-loader"
-      ]
-    },
-    {
-      "test": /\.js$/,
-      "exclude": /node_modules/,
-      "use": {
-        "loader": "babel-loader",
-        "options": {
-          "presets": [
-            "@babel/preset-env",
-          ]
-        }
-      }
-    },
-  ]
-}
+  mode: "none",
+  entry: "./src/index.js",
 
-}
+  output: {
+    path: __dirname + "/dist",
+    filename: "index.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
+  devtool: "cheap-module-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    })
+  ],
+};
